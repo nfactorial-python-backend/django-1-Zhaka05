a07956658ce9242a92203ab6aa12f436f5a6f903
Zhaka <shingis.zhansen@gmail.com>
2024-07-07 14:19:28 +0500

    3 new views

--- nfactorial/urls.py
+++ nfactorial/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", view=views.index, name="index")
+    path("", view=views.index, name="index"),
+    path("<int:first>/add/<int:second>/", view=views.add, name="add"),
+    path("transform/<str:text>/", view=views.upper, name="upper"),
+    path("check/<str:word>/", view=views.palindrome, name="palindrome"),
+    path("calc/<int:first>/<str:operation>/<int:second>/", view=views.calculate, name="calculate")
 ]
--- nfactorial/views.py
+++ nfactorial/views.py
@@ -4,3 +4,34 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, nfactorial school!")
+
+
+def add(request, first, second):
+    return HttpResponse(first + second)
+
+def upper(request, text):
+    return HttpResponse(text.upper())
+
+def palindrome(request, word):
+    def valid_palindrome(s: str) -> bool:
+        L, R = 0, len(s) - 1
+        while L < R:
+            if s[L] != s[R]:
+                return False
+            L += 1
+            R -= 1
+        return True
+    
+    return HttpResponse(valid_palindrome(word))
+
+
+def calculate(request, first, second, operation):
+    def count(val1: int, val2: int, op: str) -> int:
+        calc = {
+            "*": lambda x, y: x*y,
+            "+": lambda x, y: x+y,
+            "-": lambda x, y: x-y,
+            "/": lambda x, y: x//y,
+        }
+        return calc[op](val1, val2)
+    return HttpResponse(count(first, second, operation))
